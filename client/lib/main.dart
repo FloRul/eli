909b55b5449af4541914923a7db7f8c7a76290f4
Florian Rumiel <rumiel.florian@gmail.com>
2025-04-04 23:04:25 -0400

    Add TODO comments for additional features and management pages in main.dart

--- client/lib/main.dart
+++ client/lib/main.dart
@@ -9,9 +9,28 @@ import 'core/providers/router_provider.dart';
 // TODO: localization
 // TODO: fix the user tenant role policy to avoid admin to "demote" themselves
 
-// TODO: 1. lister toutes les fonctionnalités et valider les done et todo required --> when will beta be ready?
-// TODO: 2. stripe integration pas necessaire pour beta mais prevoir un plan de transfert de data dans la bd production
-// TODO: 3. Pland d'import de data auto ?
+// TODO: stripe integration pas necessaire pour beta mais prevoir un plan de transfert de data dans la bd production
+// TODO: Pland d'import de data auto  ou manuel ?
+
+// TODO: Contact management page + search <-- admin + member
+// TODO: Companies management page <-- admin
+// TODO: Projects management page <-- admin
+// TODO: Reminders management page <-- user (member, admin)
+// TODO: User tenant role management page  <-- admin
+
+// TODO: Deliverable view in lots list + crud view
+// TODO: Dashboard view
+
+// TODO: Breadcrumbs navigation
+// TODO: path navigation to specific lot,lot item, project, company, contact
+
+// TODO: TBD - lots and lot items filter ??
+// TODO: TBD - integrated bug/feature suggestion report ??
+// TODO: TBD - Global search bar ??
+// TODO: TBD - SUPABASE realtime subscription/broadcast
+// TODO: TBD - Custom reporting ????????
+// TODO: TBD - SUPABASE plan upgrade
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Supabase.initialize(url: SupabaseConfig.supabaseUrl, anonKey: SupabaseConfig.supabaseAnonKey);
